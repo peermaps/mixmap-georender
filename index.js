5f16f91c18697bbdc5434064c0ab25e7c4924b89
marina <marina.kukso@gmail.com>
2021-03-24 20:33:52 -1000

    updated readme. moved shaders.js to index.js

--- index.js
+++ index.js
@@ -1,2 +1,332 @@
-exports.shaders = require('./shaders.js')
-exports.prepare = require('./prepare.js')
+var glsl = require('glslify')
+var size = [0,0]
+
+module.exports = function (map) {
+  return {
+    points: {
+      frag: glsl`
+        precision highp float;
+        varying vec4 vcolor;
+        void main () {
+          if (vcolor.x < 0.1) discard;
+          gl_FragColor = vec4(vcolor);
+        }
+      `,
+      pickFrag: `
+        precision highp float;
+        varying float vfeatureType, vindex;
+        uniform float featureCount;
+        void main () {
+          gl_FragColor = vec4(vindex, vfeatureType, 0.0, 1.0);
+        }
+      `,
+      vert: glsl`
+        precision highp float;
+        #pragma glslify: Point = require('glsl-georender-style-texture/point.h');
+        #pragma glslify: readPoint = require('glsl-georender-style-texture/point.glsl');
+        uniform sampler2D styleTexture;
+        attribute vec2 position;
+        attribute float featureType, index;
+        uniform vec4 viewbox;
+        uniform vec2 offset, size;
+        uniform float featureCount, aspect, zoom;
+        varying float vfeatureType, vindex, zindex;
+        varying vec4 vcolor;
+        void main () {
+          vfeatureType = featureType;
+          Point point = readPoint(styleTexture, featureType, zoom, featureCount);
+          vcolor = point.color;
+          vindex = index;
+          zindex = point.zindex;
+          vec2 p = position.xy + offset;
+          gl_Position = vec4(
+            (p.x - viewbox.x) / (viewbox.z - viewbox.x) * 2.0 - 1.0,
+            ((p.y - viewbox.y) / (viewbox.w - viewbox.y) * 2.0 - 1.0) * aspect,
+            1.0/(1.0+zindex), 1);
+          gl_PointSize = point.size;
+        }
+      `,
+      uniforms: {
+        size: function (context) {
+          size[0] = context.viewportWidth
+          size[1] = context.viewportHeight
+          return size
+        },
+        styleTexture: map.prop('style'),
+        zoomStart: map.prop('zoomStart'),
+        zoomCount: map.prop('zoomCount'),
+        texRange: map.prop('texRange'),
+        featureCount: map.prop('styleCount')
+      },
+      attributes: {
+        position: map.prop('positions'),
+        featureType: map.prop('types'),
+        index: map.prop('indexes')
+      },
+      primitive: "points",
+      count: function (context, props) {
+        return props.positions.length/2
+      },
+      blend: {
+        enable: true,
+        func: { src: 'src alpha', dst: 'one minus src alpha' }
+      }
+    },
+    lineStroke: {
+      frag: glsl`
+        precision highp float;
+        uniform vec2 size;
+        varying vec2 vdist;
+        varying float vstrokeStyle, vstrokeDashGap;
+        varying vec4 vstrokeColor;
+        void main () {
+          float d = step(vstrokeDashGap/100.0, mod(length(vdist)*20.0, vstrokeStyle/10.0));
+          gl_FragColor = vec4(vstrokeColor.xyz, vstrokeColor.w/100.0 *
+          min(d,step(0.1,vstrokeColor.x)));
+        }
+      `,
+      pickFrag: `
+        precision highp float;
+        varying float vfeatureType, vindex;
+        uniform float featureCount;
+        void main () {
+          gl_FragColor = vec4(vindex, vfeatureType, 0.5, 1.0);
+        }
+      `,
+      vert: glsl`
+        precision highp float;
+        #pragma glslify: Line = require('glsl-georender-style-texture/line.h');
+        #pragma glslify: readLine = require('glsl-georender-style-texture/line.glsl');
+        attribute vec2 position, normal, dist;
+        attribute float featureType, index;
+        uniform vec4 viewbox;
+        uniform vec2 offset, size;
+        uniform float featureCount, aspect, zoom;
+        uniform sampler2D styleTexture;
+        varying float vfeatureType, vindex, zindex, vstrokeStyle, vstrokeDashGap;
+        varying vec2 vpos, vnorm, vdist;
+        varying vec4 vstrokeColor;
+        void main () {
+          vfeatureType = featureType;
+          Line line = readLine(styleTexture, featureType, zoom, featureCount);
+          vstrokeColor = line.strokeColor;
+          vstrokeStyle = line.strokeStyle;
+          vstrokeDashGap = line.strokeDashGap;
+          vindex = index;
+          zindex = line.zindex;
+          vec2 p = position.xy + offset;
+          vec2 m = (line.fillWidth+2.0*line.strokeWidth)/size;
+          vnorm = normalize(normal)*m;
+          vdist = vec2(
+            (dist.x / (viewbox.z - viewbox.x) * 2.0 - 1.0) * aspect,
+            (dist.y / (viewbox.w - viewbox.y) * 2.0 - 1.0) * aspect
+          );
+          gl_Position = vec4(
+            (p.x - viewbox.x) / (viewbox.z - viewbox.x) * 2.0 - 1.0,
+            ((p.y - viewbox.y) / (viewbox.w - viewbox.y) * 2.0 - 1.0) * aspect,
+            1.0/(1.0+zindex), 1);
+          vpos = gl_Position.xy;
+          gl_Position += vec4(vnorm, 0, 0);
+        }
+      `,
+      uniforms: {
+        size: function (context) {
+          size[0] = context.viewportWidth
+          size[1] = context.viewportHeight
+          return size
+        },
+        styleTexture: map.prop('style'),
+        featureCount: map.prop('styleCount'),
+        zoomStart: map.prop('zoomStart'),
+        zoomCount: map.prop('zoomCount'),
+        texRange: map.prop('texRange')
+      },
+      attributes: {
+        position: map.prop('positions'),
+        featureType: map.prop('types'),
+        index: map.prop('indexes'),
+        normal: map.prop('normals'),
+        dist: map.prop('distances')
+      },
+      primitive: "triangle strip",
+      count: function (context, props) {
+        return props.positions.length/2
+      },
+      blend: {
+        enable: true,
+        func: { src: 'src alpha', dst: 'one minus src alpha' }
+      }
+    },
+    lineFill: {
+      frag: glsl`
+        precision highp float;
+        varying float vfillStyle, vfillDashGap;
+        varying vec2 vdist;
+        varying vec4 vfillColor;
+        void main () {
+          float d = step(vfillDashGap/100.0, mod(length(vdist)*20.0, vfillStyle/10.0));
+          gl_FragColor = vec4(vfillColor.xyz, vfillColor.w/100.0 *
+          min(d,step(0.1,vfillColor.x)));
+        }
+      `,
+      pickFrag: `
+        precision highp float;
+        varying float vfeatureType, vindex;
+        uniform float featureCount;
+        void main () {
+          gl_FragColor = vec4(vindex, vfeatureType, 0.5, 1.0);
+        }
+      `,
+      vert: glsl`
+        precision highp float;
+        #pragma glslify: Line = require('glsl-georender-style-texture/line.h');
+        #pragma glslify: readLine = require('glsl-georender-style-texture/line.glsl');
+        attribute vec2 position, normal, dist;
+        attribute float featureType, index;
+        uniform vec4 viewbox;
+        uniform vec2 offset, size;
+        uniform float featureCount, aspect, zoom;
+        uniform sampler2D styleTexture;
+        varying float vfeatureType, vindex, zindex, vfillStyle, vfillDashGap;
+        varying vec2 vpos, vnorm, vdist;
+        varying vec4 vfillColor;
+        void main () {
+          vfeatureType = featureType;
+          Line line = readLine(styleTexture, featureType, zoom, featureCount);
+          vfillColor = line.fillColor;
+          vfillStyle = line.fillStyle;
+          vfillDashGap = line.fillDashGap;
+          vindex = index;
+          zindex = line.zindex + 0.1;
+          vec2 p = position.xy + offset;
+          vnorm = normalize(normal)*(line.fillWidth/size);
+          vdist = vec2(
+            (dist.x / (viewbox.z - viewbox.x) * 2.0 - 1.0) * aspect,
+            (dist.y / (viewbox.w - viewbox.y) * 2.0 - 1.0) * aspect
+          );
+          gl_Position = vec4(
+            (p.x - viewbox.x) / (viewbox.z - viewbox.x) * 2.0 - 1.0,
+            ((p.y - viewbox.y) / (viewbox.w - viewbox.y) * 2.0 - 1.0) * aspect,
+            1.0/(1.0+zindex), 1);
+          vpos = gl_Position.xy;
+          gl_Position += vec4(vnorm, 0, 0);
+        }
+      `,
+      uniforms: {
+        size: function (context) {
+          size[0] = context.viewportWidth
+          size[1] = context.viewportHeight
+          return size
+        },
+        styleTexture: map.prop('style'),
+        featureCount: map.prop('styleCount'),
+        zoomStart: map.prop('zoomStart'),
+        zoomCount: map.prop('zoomCount'),
+        texRange: map.prop('texRange')
+      },
+      attributes: {
+        position: map.prop('positions'),
+        featureType: map.prop('types'),
+        index: map.prop('indexes'),
+        normal: map.prop('normals'),
+        dist: map.prop('distances')
+      },
+      primitive: "triangle strip",
+      count: function (context, props) {
+        return props.positions.length/2
+      },
+      blend: {
+        enable: true,
+        func: { src: 'src alpha', dst: 'one minus src alpha' }
+      }
+    },
+    areas: {
+      frag: glsl`
+        precision highp float;
+        varying vec4 vcolor;
+        void main () {
+          gl_FragColor = vec4(vcolor);
+        }
+      `,
+      pickFrag: `
+        precision highp float;
+        varying float vfeatureType, vindex;
+        uniform float featureCount;
+        void main () {
+          gl_FragColor = vec4(vindex, vfeatureType, 1.0, 1.0);
+        }
+      `,
+      vert: glsl`
+        precision highp float;
+        #pragma glslify: Area = require('glsl-georender-style-texture/area.h');
+        #pragma glslify: readArea = require('glsl-georender-style-texture/area.glsl');
+        attribute vec2 position;
+        attribute float featureType, index;
+        uniform vec4 viewbox;
+        uniform vec2 offset, size, texRange;
+        uniform float aspect, featureCount, zoom, zoomStart, zoomCount;
+        uniform sampler2D styleTexture;
+        varying float vfeatureType, vindex, zindex;
+        varying vec4 vcolor;
+        void main () {
+          vfeatureType = featureType;
+          Area area = readArea(styleTexture, featureType, zoom, featureCount);
+          vcolor = area.color;
+          vindex = index;
+          zindex = area.zindex;
+          vec2 p = position.xy + offset;
+          gl_Position = vec4(
+            (p.x - viewbox.x) / (viewbox.z - viewbox.x) * 2.0 - 1.0,
+            ((p.y - viewbox.y) / (viewbox.w - viewbox.y) * 2.0 - 1.0) * aspect,
+            1.0/(1.0+zindex), 1);
+        }
+      `,
+      uniforms: {
+        size: function (context) {
+          size[0] = context.viewportWidth
+          size[1] = context.viewportHeight
+          return size
+        },
+        featureCount: map.prop('styleCount'),
+        styleTexture: map.prop('style'),
+        zoomStart: map.prop('zoomStart'),
+        zoomCount: map.prop('zoomCount'),
+        texRange: map.prop('texRange')
+      },
+      attributes: {
+        position: map.prop('positions'),
+        featureType: map.prop('types'),
+        index: map.prop('indexes')
+      },
+      elements: map.prop('cells'),
+      primitive: "triangles",
+      blend: {
+        enable: true,
+        func: {
+          src: 'src alpha',
+          dst: 'one minus src alpha',
+          rgb: 'add',
+          alpha: 'max'
+        }
+      }
+    },
+    labels: {
+			frag: `
+			precision mediump float;
+			void main () {
+				gl_FragColor = vec4(0,0,1,1);
+			}`,
+			vert: `
+			precision mediump float;
+			attribute vec2 position;
+			void main () {
+				gl_Position = vec4(position.xy*vec2(1,-1)*0.2, 0, 1);
+			}`,
+			attributes: {
+				position: map.prop('positions')
+      },
+			elements: map.prop('cells'),
+			depth: { enable: false }
+    }
+  }
+}
--- readme.md
+++ readme.md
@@ -1,25 +1,104 @@
-***IN PROGRESS***
+# mixmap-georender
 
-
-a mixmap layer for rendering open street map data from peermaps.
-
-
-# installation
-
-in your terminal, run `npm install` after you've cloned the repo.
+a mixmap layer for rendering peermaps georender data.
 
 # example
 
-first, open the terminal and navigate to the directory where you've cloned your
-repo. go to the
-`example` directory. once there, run `tar -xzvf geodata.tar.gz`. after that, the
-`example` directory should have a file called `points.json` and `lines.json`.
+```js
+var mixmap = require('mixmap')
+var regl = require('regl')
+var prepare = require('mixmap-georender/prepare')
+var decode = require('georender-pack/decode')
+var lpb = require('length-prefixed-buffers')
+ 
+var mix = mixmap(regl, { extensions: [
+  'oes_element_index_uint', 'oes_texture_float', 'EXT_float_blend' ] })
+var map = mix.create({ 
+  viewbox: [+36.2146, +49.9962, +36.2404, +50.0154],
+  backgroundColor: [0.82, 0.85, 0.99, 1.0],
+  pickfb: { colorFormat: 'rgba', colorType: 'float32' }
+})
+var geoRender = require('mixmap-georender')(map)
+ 
+var draw = {
+  area: map.createDraw(geoRender.areas),
+  lineStroke: map.createDraw(geoRender.lineStroke),
+  lineFill: map.createDraw(geoRender.lineFill),
+  point: map.createDraw(geoRender.points),
+}
 
-then, do `npm run example`. if everything worked correctly, you
-should get some output that includes the url. you should have a line like this:
+var ready = function (props) {
+  draw.point.props.push(props.point)
+  draw.lineFill.props.push(props.lineFill)
+  draw.lineStroke.props.push(props.lineStroke)
+  draw.area.props.push(props.area)
+  map.draw()
+}
 
-`[0005] info  Server running at http://192.168.129.37:9966/ (connect)`
+require('resl')({
+  manifest: {
+    texture: {
+      type: 'image',
+      src: './example/style.png',
+      parser: function (data) { return map.regl.texture({ data }) }
+    },
+    buffers: {
+      type: 'binary',
+      src: './example/kharkiv',
+      parser: function (data) { return lpb.decode(Buffer.from(data)) }
+    }
+  },
+  onDone: function ({texture, buffers}) {
+    ready(prepare(decode(buffers), texture))
+  }
+})
 
-if you navigate to that url in your browser and click in the website window, you
-should eventually see a map. (for now, you have to click in the window -
-this will be fixed.)
+window.addEventListener('keydown', function (ev) {
+  if (ev.code === 'Digit0') {
+    map.setZoom(Math.min(6,Math.round(map.getZoom()+1)))
+  } else if (ev.code === 'Minus') {
+    map.setZoom(map.getZoom()-1)
+  } else if (ev.code === 'Equal') {
+    map.setZoom(map.getZoom()+1)
+  }
+})
+
+window.addEventListener('resize', function (ev) {
+  map.resize(window.innerWidth, window.innerHeight)
+})
+ 
+document.body.appendChild(mix.render())
+document.body.appendChild(map.render({ width: window.innerWidth, height: window.innerHeight }))
+```
+
+to run example, [details about getting the style.png and kharkiv buf file]
+
+# api
+
+```
+var mixmapGeorender = require('mixmap-georender')
+var prepare = require('mixmap-georender/prepare')
+```
+
+## var geoRender = mixmapGeorender(map)
+
+create a new instance of mixmap-georender. `map` is a
+[mixmap](https://github.com/peermaps/mixmap) instance created with
+[`mix.create`](https://github.com/peermaps/mixmap#var-map--mixcreateopts)
+
+## prepare(decoded, texture)
+
+given `decoded`, an object created by [georender-pack
+decode](https://github.com/peermaps/georender-pack/#decode) and a style texture,
+return an object that you can pass to `map.draw()`.
+
+the texture used here is created with
+[georender-style2png](https://www.npmjs.com/package/georender-style2png).
+
+# install
+
+`npm install mixmap-georender`
+
+# license
+
+MIT
