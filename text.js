f7e839ff041e01fcc71a9b692adc1195ea2ffad8
substack <substack@bits.coop>
2022-04-30 18:43:28 -1000

    some line rendering

--- text.js
+++ text.js
@@ -5,14 +5,6 @@ var labelPreset = {
   point: require('label-placement-engine/preset/point'),
   line: require('label-placement-engine/preset/line'),
 }
-var labelTypes = {
-  pointP: 'point',
-  pointT: 'point',
-  lineP: 'line',
-  lineT: 'line',
-  areaP: 'bbox',
-  areaT: 'bbox',
-}
 
 module.exports = Text
 
@@ -38,6 +30,13 @@ function Text(opts) {
         pointMargin: [10,10],
         pointSeparation: [10,10],
       }),
+      line: labelPreset.line({
+        labelMargin: [10,10],
+        lineSize: [10,10],
+        lineMargin: [10,10],
+        lineSeparation: [10,10],
+        sides: ['center','left','right'],
+      }),
     }
   })
   this._props = {}
@@ -50,43 +49,10 @@ Text.prototype.update = function (props, map) {
   var labels = []
   labels.push({ type: 'bbox', bounds: map.viewbox })
   var ph = 4, pw = 4
-  for (var key in props) {
-    if (!props.hasOwnProperty(key)) continue
-    var type = labelTypes[key]
-    if (!/^point/.test(key)) continue
-    var p = props[key]
-    if (!p || !p.positions) continue
-    for (var ix = 0; ix < p.id.length; ix++) {
-      var id = p.id[ix]
-      if (!p.labels.hasOwnProperty(id) || p.labels[id].length === 0) continue
-      // only the first one right now
-      var text = p.labels[id][0].replace(/^[^=]*=/,'')
-      var lon = p.positions[ix*2+0]
-      var lat = p.positions[ix*2+1]
-      if (map.viewbox[0] > lon || lon > map.viewbox[2]) continue
-      if (map.viewbox[1] > lat || lat > map.viewbox[3]) continue
-      var pxToLon = (map.viewbox[2]-map.viewbox[0]) / map._size[0]
-      var pxToLat = (map.viewbox[3]-map.viewbox[1]) / map._size[1]
-      var lonPx = lon / (map.viewbox[2]-map.viewbox[0]) * map._size[0]
-      var latPx = lat / (map.viewbox[3]-map.viewbox[1]) * map._size[1]
-      var m = this._ctx.measureText(text)
-      var widthPx = Math.ceil(m.actualBoundingBoxRight - m.actualBoundingBoxLeft)
-      var heightPx = Math.ceil(m.actualBoundingBoxAscent + m.actualBoundingBoxDescent)
-      var widthLon = (widthPx + pw + 1) * pxToLon
-      var heightLat = (heightPx + ph + 1) * pxToLat
-      labels.push({
-        type: 'point',
-        point: [lon,lat],
-        labelSize: [widthLon,heightLat],
-        labelMargin: [10/map._size[0]*widthLon,10/map._size[1]*heightLat],
-        pointSize: [10/map._size[0]*widthLon,10/map._size[1]*heightLat],
-        pointMargin: [10/map._size[0]*widthLon,10/map._size[1]*heightLat],
-        widthPx,
-        heightPx,
-        text,
-      })
-    }
-  }
+  this._addPoint(map, labels, props.pointT, pw, ph)
+  this._addPoint(map, labels, props.pointP, pw, ph)
+  this._addLine(map, labels, props.lineT)
+  this._addLine(map, labels, props.lineP)
   this._labelEngine.update(labels)
 
   var uvs = null
@@ -102,7 +68,7 @@ Text.prototype.update = function (props, map) {
   var bins = []
   for (var i = 0; i < labels.length; i++) {
     var x = labels[i]
-    if (x.type === 'point' && this._labelEngine.visible[i] > 0.5) {
+    if ((x.type === 'point' || x.type === 'line') && this._labelEngine.visible[i] > 0.5) {
       bins.push({ i, width: x.widthPx+pw, height: x.heightPx+ph })
     }
   }
@@ -150,4 +116,85 @@ Text.prototype._setFont = function () {
   this._ctx.fillStyle = this._fillStyle
   this._ctx.lineWidth = this._lineWidth
   this._ctx.strokeStyle = this._strokeStyle
+}
+
+Text.prototype._addPoint = function (map, labels, p, pw, ph) {
+  if (!p || !p.positions) return
+  for (var ix = 0; ix < p.id.length; ix++) {
+    var id = p.id[ix]
+    if (!p.labels.hasOwnProperty(id) || p.labels[id].length === 0) continue
+    // only the first one right now
+    var text = p.labels[id][0].replace(/^[^=]*=/,'')
+    var lon = p.positions[ix*2+0]
+    var lat = p.positions[ix*2+1]
+    if (map.viewbox[0] > lon || lon > map.viewbox[2]) continue
+    if (map.viewbox[1] > lat || lat > map.viewbox[3]) continue
+    var pxToLon = (map.viewbox[2]-map.viewbox[0]) / map._size[0]
+    var pxToLat = (map.viewbox[3]-map.viewbox[1]) / map._size[1]
+    var m = this._ctx.measureText(text)
+    var widthPx = Math.ceil(m.actualBoundingBoxRight - m.actualBoundingBoxLeft)
+    var heightPx = Math.ceil(m.actualBoundingBoxAscent + m.actualBoundingBoxDescent)
+    var widthLon = (widthPx + pw + 1) * pxToLon
+    var heightLat = (heightPx + ph + 1) * pxToLat
+    labels.push({
+      type: 'point',
+      point: [lon,lat],
+      labelSize: [widthLon,heightLat],
+      labelMargin: [10/map._size[0]*widthLon,10/map._size[1]*heightLat],
+      pointSize: [10/map._size[0]*widthLon,10/map._size[1]*heightLat],
+      pointMargin: [10/map._size[0]*widthLon,10/map._size[1]*heightLat],
+      widthPx,
+      heightPx,
+      text,
+    })
+  }
+}
+
+Text.prototype._addLine = function (map, labels, l) {
+  var ph = 4, pw = 4
+  if (!l || !l.positions) return
+  var start = 0, prev = null
+  for (var ix = 0; ix < l.id.length; ix++) {
+    var id = l.id[ix]
+    if ((prev === null || prev === id) && ix !== l.id.length-1) {
+      prev = id
+      continue
+    }
+    prev = id
+    if (!l.labels.hasOwnProperty(id) || l.labels[id].length === 0) {
+      start = ix
+      continue
+    }
+    // only the first one right now
+    var text = l.labels[id][0].replace(/^[^=]*=/,'')
+    var end = ix
+    var vb = map.viewbox
+    var positions = []
+    for (var i = start*2; i < end*2+2; i+=2) {
+      var x = l.positions[i+0], y = l.positions[i+1]
+      if (vb[0] <= x && x <= vb[2] && vb[1] <= y && y <= vb[3]) {
+        positions.push(x,y)
+      }
+    }
+    start = ix
+    var pxToLon = (map.viewbox[2]-map.viewbox[0]) / map._size[0]
+    var pxToLat = (map.viewbox[3]-map.viewbox[1]) / map._size[1]
+    var m = this._ctx.measureText(text)
+    var widthPx = Math.ceil(m.actualBoundingBoxRight - m.actualBoundingBoxLeft)
+    var heightPx = Math.ceil(m.actualBoundingBoxAscent + m.actualBoundingBoxDescent)
+    var widthLon = (widthPx + pw + 1) * pxToLon
+    var heightLat = (heightPx + ph + 1) * pxToLat
+    labels.push({
+      type: 'line',
+      positions,
+      side: 'center',
+      labelSize: [widthLon,heightLat],
+      labelMargin: [10/map._size[0]*widthLon,10/map._size[1]*heightLat],
+      lineSize: [10/map._size[0]*widthLon,10/map._size[1]*heightLat],
+      lineMargin: [10/map._size[0]*widthLon,10/map._size[1]*heightLat],
+      widthPx,
+      heightPx,
+      text,
+    })
+  }
 }
